b0ff1cab48e6ef6e18cda4e80301743d2dc3dd9e
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 19:08:35 +0530

    Add files via upload

---
+++ Star_Pattern.cpp
@@ -0,0 +1,15 @@
+//Sakshi sonawane 
+// PRN:24070123505
+//Implementation Of Star Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<5;i++){
+        for(j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
