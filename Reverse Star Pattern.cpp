8388021a212d0a61b2b939f5cd3ecaac57c50cc9
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 19:05:29 +0530

    Add files via upload

---
+++ Reverse Star Pattern.cpp
@@ -0,0 +1,13 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+int main() {
+    int i, j;
+    for (i = 5; i > 0; i--) {
+        for (j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
