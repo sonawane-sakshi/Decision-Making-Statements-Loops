ede708082fa65affbd6b212a6f03dbdfb02703e6
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 18:54:37 +0530

    Add files via upload

---
+++ Do_While.cpp
@@ -0,0 +1,11 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+int main() {
+    string name;
+    do {
+        cout << "Enter the string" << endl;
+        cin >> name;
+    } while (name != "STOP"); 
+}
